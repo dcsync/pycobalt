d422c4edc578c33040036b917d2d4f89028a5b3c
dcsync <dcsync@protonmail.com>
2018-10-26 20:23:31 +0000

    .

--- utils.py
+++ utils.py
@@ -3,27 +3,16 @@
 #
 
 import inspect
+import os
 
-# for verifying alias and command arg types
-def verify_args(args, arg_types):
-    # do not verify
-    if arg_types == None:
-        return True
-
-    # check length
-    if len(args) != len(arg_types):
-        return False
-
-    # check types
-    for arg, arg_type in zip(args, arg_types):
-        if not isinstance(arg, arg_type):
-            return False
-
-    # all good
-    return True
-
-# Check arguments before passing to function
 def check_args(func, args):
+    """
+    Check argument list length before calling a function
+
+    For functions with *args there is no maximum argument length. The minimum
+    argument length is the number of positional and keyword arguments a
+    function has.
+    """
     sig = inspect.signature(func)
     min_args = 0
     max_args = len(sig.parameters)
@@ -32,11 +21,20 @@ def check_args(func, args):
             # no max arg
             max_args = 9999
         else:
+            # positional, kwarg, etc
             if not info.default:
                 min_args += 1
 
     return len(args) >= min_args and len(args) <= max_args
 
-# Get function signature
 def signature(func):
+    """
+    Get stringy function argument signature
+    """
     return str(inspect.signature(func))
+
+def basedir(append='', relative=__file__):
+    """
+    Get base directory relative to 'relative' or __file__
+    """
+    return os.path.realpath(os.path.dirname(relative)) + '/' + append
