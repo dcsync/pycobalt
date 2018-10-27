0baf4152579f3c0be21cc28b34f883dc5cb1f0b1
dcsync <dcsync@protonmail.com>
2018-10-27 18:12:52 +0000

    .

---
+++ pycobalt/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name = 'pycobalt',
+    description = 'Python API for Cobaltstrike',
+)
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-{
-	echo '{"name": "event", "message": {"name": "event_action", "args": ["a", "b", "c"]}}'
-	echo '{"name": "command", "message": {"name": "test", "args": []}}'
-	echo '{"name": "alias", "message": {"name": "test", "args": []}}'
-} |
-./test.py
