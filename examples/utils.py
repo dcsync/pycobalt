0f88dec7e849c83dd4629f1b0aa85abbd664beb6
Your Name <you@example.com>
2018-10-28 01:15:44 +0000

    .

---
+++ examples/utils.py
@@ -0,0 +1,11 @@
+#
+# Utils
+#
+
+import os
+
+def basedir(append='', relative=__file__):
+    """
+    Get base directory relative to 'relative' or __file__
+    """
+    return os.path.realpath(os.path.dirname(relative)) + '/' + append
